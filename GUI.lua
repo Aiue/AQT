b82b9b9b5ad6f91ef216ba96983ff395ed54eb8b
Aiue-ScarshieldLegion <aiue@aiue.se>
2019-09-06 10:14:51 +0200

    Apart from minor tweakings later on, GUI part should be mostly done.

--- GUI.lua
+++ GUI.lua
@@ -11,7 +11,7 @@ local recycler = {}
 local function getAvailableName(name)
    if not _G[name] then return name else
       local count = string.match(name, "%d+")
-      if not count then count = 0 end
+      if not count then count = 0 end -- !!!RE!!! Cache this to avoid lots of recursions when we already have a whole bunch of frames.
       count = count + 1
       return getAvailableName(string.sub(name, string.find(name, "%a+")) .. tostring(count))
    end
@@ -24,18 +24,33 @@ gui.content = {} --???
 local guiFunc = {}
 
 function gui:OnEnable()
-   gui:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -10, -250)
+   gui:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", st.cfg.posX, st.cfg.posY)
 
    local padding = 2 --or whatever the config says
 
+   gui.font = CreateFont(getAvailableName("AQTFont"))
+   gui.font:SetJustifyV("TOP")
+   gui.font:SetFont(LSM:Fetch("font", st.cfg.font.name), st.cfg.font.size)
+   gui.font:SetShadowColor(st.cfg.font.shadow.r, st.cfg.font.shadow.g, st.cfg.font.shadow.b, st.cfg.font.shadow.a)
+   gui.font:SetShadowOffset(st.cfg.font.shadow.x, st.cfg.font.shadow.y)
+   gui.font:SetSpacing(st.cfg.font.spacing)
+   gui.font:SetTextColor(st.cfg.font.r, st.cfg.font.g, st.cfg.font.b, st.cfg.font.a)
+
    gui.scrollFrame = CreateFrame("ScrollFrame", getAvailableName("AQTScrollFrame"), gui)
-   gui.scrollFrame:SetPoint("TOPLEFT", gui, "TOPLEFT", padding, -padding)
-   gui.scrollFrame:SetPoint("BOTTOMRIGHT", gui, "BOTTOMRIGHT", -padding, padding)
+   gui.scrollFrame:SetPoint("TOPLEFT", gui, "TOPLEFT", st.cfg.padding, -st.cfg.padding)
+   gui.scrollFrame:SetPoint("BOTTOMRIGHT", gui, "BOTTOMRIGHT", -st.cfg.padding, st.cfg.padding)
 
    gui.scrollChild = CreateFrame("Frame", getAvailableName("AQTScrollChild"), gui.scrollFrame)
+   gui.scrollFrame:SetScrollChild(gui.scrollChild)
    gui.scrollChild:SetPoint("TOPLEFT", gui.scrollFrame)
    gui.scrollChild:SetPoint("TOPRIGHT", gui.scrollFrame)
-   gui.scrollFrame:SetScrollChild(gui.scrollChild)
+
+   function gui.scrollChild:UpdateSize()
+      local h = gui.title:GetHeight() + gui.title.container:GetHeight()
+      gui.scrollChild:SetHeight(h)
+
+      gui:SetHeight((h + st.cfg.padding*2) > st.cfg.maxHeight and st.cfg.maxHeight or (h + st.cfg.padding*2))
+   end
 
    local backdrop = {
       bgFile = LSM:Fetch("background", st.cfg.backdrop.background.name),
@@ -53,18 +68,20 @@ function gui:OnEnable()
    gui:SetBackdrop(backdrop)
    gui:SetBackdropColor(st.cfg.backdrop.background.r, st.cfg.backdrop.background.g, st.cfg.backdrop.background.b, st.cfg.backdrop.background.a)
    gui:SetBackdropBorderColor(st.cfg.backdrop.border.r, st.cfg.backdrop.border.g, st.cfg.backdrop.border.b, st.cfg.backdrop.border.a)
-end
 
---function f:newAnchor(name)
---   anchor:UpdateBackdrop()
---   anchor:SetWidth(st.cfg.profile.frames[name].minWidth)
---   anchor:SetHeight(padding*2)
---   anchor.scrollFrame:GetScrollChild():SetHeight(0) -- not sure if this will be proper or naeh
---end
+   gui:SetWidth(st.cfg.maxWidth) --!!!RE!!!
+
+   gui.title = guiFunc.New(gui.scrollChild)
+   gui.title.text:SetText("AQT")
+   gui.title.counter:SetText("|cff00ff000/20|r")
+   gui.title:SetPoint("TOPLEFT", gui.scrollChild, "TOPLEFT")
+   gui.title:SetPoint("TOPRIGHT", gui.scrollChild, "TOPRIGHT")
+   gui.title:Update()
+end
 
 local mt = {
    __index = function(t, k)
-      return guiFunc[k]
+      if guiFunc[k] then return guiFunc[k] else return getmetatable(gui).__index[k] end --!!!RE!!!
    end
 }
 
@@ -82,6 +99,7 @@ function guiFunc:Release(recursed)
    self.text:SetText("")
    self.counter:SetText("")
    self.container:SetHeight(0)
+   self.container:Show()
 
    for k,v in ipairs(parent.children) do
       if self == v then tinsert(recycler, tremove(parent.children, k)) end
@@ -107,14 +125,16 @@ function guiFunc:New()
       object = tremove(recycler)
       object.container:Show()
    else
-      object = CreateFrame("Frame", getAvailableName("AQTRow"))
+      object = CreateFrame("Frame", getAvailableName("AQTRow"), self)
       object.button = CreateFrame("Button", getAvailableName("AQTButton"), object)
       object.button:SetPoint("TOPLEFT", object)
       object.button:SetSize(16,16)
-      object.text = CreateFontString(getAvailableName("AQTText"), object)
-      object.text = SetJustifyH("LEFT")
+      object.text = object:CreateFontString(getAvailableName("AQTText"), object)
+      object.text:SetFontObject(gui.font)
+      object.text:SetJustifyH("LEFT")
       object.text:SetPoint("TOPLEFT", object.button, "TOPRIGHT")
-      object.counter = CreateFontString(getAvailableName("AQTCounter"), object)
+      object.counter = object:CreateFontString(getAvailableName("AQTCounter"), object)
+      object.counter:SetFontObject(gui.font)
       object.counter:SetJustifyH("RIGHT")
       object.counter:SetPoint("TOPRIGHT", object)
       object.text:SetPoint("TOPRIGHT", object.counter, "TOPLEFT")
@@ -122,10 +142,9 @@ function guiFunc:New()
       object.children = {}
       setmetatable(object, mt)
    end
-   object:SetParent(self)
    object:ButtonCheck()
-   tinsert(self.children, object)
-   self:Update()
+   tinsert(object, self.children)
+   if self ~= gui.scrollChild then self:Update() end
    return object
 end
 
@@ -165,6 +184,9 @@ function guiFunc:UpdateSize()
    end
 
    self.container:SetHeight(h)
-   
-   if self:GetParent() ~= UIParent then self:GetParent():UpdateSize() end --change this, eventually "gui" is going to be the parent, and that will be the base case.
+
+   local th,ch = self.text:GetHeight(),self.counter:GetHeight()
+   self:SetHeight(th > ch and th or ch)
+
+   self:GetParent():UpdateSize() -- gui.scrollChild will have its own function, so no need for a base case
 end
