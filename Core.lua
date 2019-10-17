fc4bc6a30e203bcb8fa78580346dea52b223c724
Jens Nilsson Sahlin <felibarr@gmail.com>
2019-10-17 15:25:37 +0200

    Add support for Classic Quest Log (#39).

--- Core.lua
+++ Core.lua
@@ -103,7 +103,7 @@ local Quest = baseObject:New(
 		     break
 		  end
 	       end
-	       local frame = QuestLogExFrame or QuestLogFrame
+	       local frame = ClassicQuestLog or QuestLogExFrame or QuestLogFrame
 	       if (frame:IsShown() and GetQuestLogIndexByID(self.id) == GetQuestLogSelection()) or not frame:IsShown() then ToggleQuestLog() end
 	       local index = GetQuestLogIndexByID(self.id)
 	       QuestLog_SetSelection(index)
