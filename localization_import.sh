7342ed69c1fab6694d3c361127d6db0e87ee50fe
Jens Nilsson Sahlin <felibarr@gmail.com>
2019-10-28 17:23:46 +0100

    Test #n+1.

--- localization_import.sh
+++ localization_import.sh
@@ -10,7 +10,7 @@ trap 'rm -f $tempfile' EXIT
 lua LocaleParser.lua || exit 1
 
 result=$( curl -sS -0 -X POST -w "%{http_code}" -o "$tempfile" -H "X-Api-Token: $CF_API_KEY" \
-    -F "metadata={ language: \"enUS\", missing-phrase-handling: \"DeletePhrase\" }" \
+    -F "metadata={ language: \"enUS\", \"missing-phrase-handling\": \"DeletePhrase\" }" \
     -F "localizations=<L.lua" "https://wow.curseforge.com/api/projects/67669/localization/import") || exit 1
 
 case $result in
