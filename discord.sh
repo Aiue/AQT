c82c144bb232fd87849e3b13164d1b3fdb0c6c6d
Jens Nilsson Sahlin <felibarr@gmail.com>
2019-11-01 05:48:47 +0100

    Move discord announce from yaml to its own shell script.

---
+++ discord.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"Release\", \"content\": \"New release: [AQT-${TRAVIS_TAG}](<https://github.com/Aiue/AQT/releases/tag/${TRAVIS_TAG}>). Get it on [CurseForge](<https://www.curseforge.com/wow/addons/aqt>) or [WoWInterface](<https://www.wowinterface.com/downloads/fileinfo.php?id=25280>)\\\!\"}" $WEBHOOK
+
